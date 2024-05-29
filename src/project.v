2c66aad2d2a54caa647c2579efcd090375cf49d4
sreeramtirumalasetty <146643242+sreeramtirumalasetty@users.noreply.github.com>
2024-05-29 23:36:56 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,7 +10,7 @@
 `include "booth_encoding.v"
 `include "latchh.v"
 
-module tt_um_sreeramtirumalasetty (
+module tt_um_multiplier_mbm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
