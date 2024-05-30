2803ee3cfee52484eacd9c5b78c27cdf908d1d8e
sreeramtirumalasetty <146643242+sreeramtirumalasetty@users.noreply.github.com>
2024-05-30 21:04:26 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,17 +11,17 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 0.01, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # # Reset
+    # dut._log.info("Reset")
+    # dut.ena.value = 0
+    # dut.ui_in.value = 0
+    # dut.uio_in.value = 0
+    # dut.rst_n.value = 0
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst_n.value = 0
 
     dut._log.info("Test project behavior")
 
